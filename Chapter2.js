061b260f0b4b6e0da4696d96e404ba7e9d22b388
rvprakash9596 <raviprakash9596@gmail.com>
2024-08-04 12:36:30 +0530

    Notes

---
+++ Chapter2.js
@@ -0,0 +1,34 @@
+// JS Lecture 2
+// const a="hello bhai kaise hai tu.."; //string'
+
+//Single line comments
+
+/* 
+multi-line comments
+This code prints Hello JS
+*/
+
+// console.log("Hello JS");
+
+// Arithmetic Operators
+let a=5;
+let b=2;
+console.log("a =",a,"\nb =",b);
+// console.log(a+b);
+console.log(" a + b = ",a+b);
+console.log(" a - b = ",a-b);
+console.log(" a * b = ",a*b);
+console.log(" a / b = ",a/b);
+console.log(" a % b = ",a%b);
+console.log(" a ** b = ",a**b);//Exponentiation
+
+// Unary Operator
+
+console.log(++a);// 6 pre-increment operator
+console.log(a++);// 6post-increment operator
+console.log(a); //7
+
+console.log(a--); // 7post-decrement
+console.log(--a); // 5 pre-decrement
+console.log(a);//5
+
